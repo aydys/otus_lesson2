7d5e1308cfa52b67ffd5bac762613cadc81e7018
aidys <aidongak23@gmail.com>
2021-01-28 09:19:56 +0700

    add babel & webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+}
