1540c043c501544a46fe02c6047366c7d10c225e
aidys <aidongak23@gmail.com>
2021-01-29 13:33:16 +0700

    forman webpack config via prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'source-map',
+  entry: "./src/index.ts",
+  devtool: "source-map",
   resolve: {
-    extensions: ['.ts']
+    extensions: [".ts"],
   },
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.js'
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|ts)x?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
-    historyApiFallback: true
-  }
-}
+    historyApiFallback: true,
+  },
+};
