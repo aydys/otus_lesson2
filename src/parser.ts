2aa4374e73295dfa4210bf387d39b1392bd22074
aidys <aidongak23@gmail.com>
2021-01-29 13:37:06 +0700

    add parser

---
+++ src/parser.ts
@@ -0,0 +1,30 @@
+import { mathOperators } from "./mathOperators";
+import { isNumber } from "./helpers";
+
+export type ParsedLineType = Array<number | string>;
+
+export const parser = (line: string): ParsedLineType | null => {
+  const stack = line.split(" ");
+
+  const result = stack.reduce((accum, val, ind, arr) => {
+    const prevItem = arr[ind - 1];
+    const item = arr[ind];
+
+    const isValidNumberPush = !isNumber(prevItem) && isNumber(item);
+    const isValidOperatorPush =
+      isNumber(prevItem) &&
+      !isNumber(item) &&
+      mathOperators.hasOwnProperty(item);
+
+    if (isValidNumberPush) {
+      accum.push(Number(item));
+    } else if (isValidOperatorPush) {
+      accum.push(item);
+    } else {
+      throw new TypeError("Unexpected string");
+    }
+    return accum;
+  }, [] as (string | number)[]);
+
+  return result;
+};
